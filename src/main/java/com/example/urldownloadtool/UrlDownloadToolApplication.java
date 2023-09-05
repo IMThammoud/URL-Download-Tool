70e662453f91a5cdd6780a921c0f57d28d574bda
IMThammoud <hahammoud@mailbox.org>
2023-09-05 11:23:51 +0200

    Added a schedule that cycles every 10 minutes

--- src/main/java/com/example/urldownloadtool/UrlDownloadToolApplication.java
+++ src/main/java/com/example/urldownloadtool/UrlDownloadToolApplication.java
@@ -1,13 +1,17 @@
 package com.example.urldownloadtool;
 
+import com.example.urldownloadtool.scheduled.ScheduledTasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class UrlDownloadToolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlDownloadToolApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/example/urldownloadtool/scheduled/ScheduledTasks.java
+++ src/main/java/com/example/urldownloadtool/scheduled/ScheduledTasks.java
@@ -1,5 +1,37 @@
 package com.example.urldownloadtool.scheduled;
 
-public class ScheduledTasks {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import java.io.File;
+
+@Component
+public class ScheduledTasks {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+    // Set Path to video folder
+    private static final String userDir = System.getProperty("user.dir");
+    private static final String videoFolder= userDir+ "/videos/";
+
+    // This scheduled Task runs every 10 Minutes and deletes videos from the folder
+    @Scheduled(fixedRate = 600000)
+    public void cleanUpVideos(){
+        File videoDir = new File(videoFolder);
+
+        // returns the files in the video directory
+        File[] videosInFolder = videoDir.listFiles();
+
+        // goes through every video in the folder and deletes it on schedule
+        if (videosInFolder != null) {
+            for (File file : videosInFolder
+            ) {
+                if (file.isFile()) {
+                    log.info("current File deleted:" + file.toString());
+                    file.delete();
+                }
+
+            }
+        }
+    }
 }
